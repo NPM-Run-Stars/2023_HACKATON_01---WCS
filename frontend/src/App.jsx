daf05ecfa91e0ecb82c2785f19192b2624997757
AikoFr <aikootv@gmail.com>
2023-05-11 11:59:42 +0200

    Prepa

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Error from "./pages/Error";
+// Page2
+
+// Page3
+
+// Page4
+
+// Page5
 
 import "./App.css";
 
@@ -10,7 +17,16 @@ function App() {
       <Router>
         <Routes>
           <Route path="*" element={<Error />} />
+          {/* Page 1 */}
           <Route path="/" element={<Home />} />
+
+          {/* Page 2 */}
+
+          {/* Page 3 */}
+
+          {/* Page 4 */}
+
+          {/* Page 5 */}
         </Routes>
       </Router>
     </div>
