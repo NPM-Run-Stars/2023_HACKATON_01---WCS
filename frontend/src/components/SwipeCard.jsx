f4fcb76234bd90df23750e39293580fb38f989d2
Marta Miszke <mishke.design@gmail.com>
2023-05-11 21:33:04 +0200

    desig card swipe

--- frontend/src/components/SwipeCard.jsx
+++ frontend/src/components/SwipeCard.jsx
@@ -3,21 +3,25 @@ import TinderCard from "react-tinder-card";
 
 function SwipeCard({ userprofile, i }) {
   return (
-    <TinderCard>
-      <div className="card-container">
-        <div className="profile-container" style={{ zIndex: -i + 1 }}>
-          <img src={userprofile.image_pr} alt={userprofile.nom} />
-          <h2>
-            {userprofile.prenom} {userprofile.nom}
-          </h2>
-          <p>{userprofile.age}</p>
-          <p>{userprofile.dob}</p>
-          <p>{userprofile.email}</p>
-          <p>{userprofile.genre}</p>
-          <p>{userprofile.phone}</p>
+    <div className="bg-card">
+      <TinderCard>
+        <div className="card-container">
+          <div className="profile-container" style={{ zIndex: -i + 1 }}>
+            <img src={userprofile.image_pr} alt={userprofile.nom} />
+            <div className="profile-descript">
+              <h2>
+                {userprofile.prenom} {userprofile.nom}
+              </h2>
+              <p>{userprofile.age}</p>
+              <p>{userprofile.dob}</p>
+              <p>{userprofile.email}</p>
+              <p>{userprofile.genre}</p>
+              <p>{userprofile.phone}</p>
+            </div>
+          </div>
         </div>
-      </div>
-    </TinderCard>
+      </TinderCard>
+    </div>
   );
 }
 
