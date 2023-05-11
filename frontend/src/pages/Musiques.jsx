e0c6ef30d2a960aaf01fb1d49b76e68fa9cb1eee
Cyrielle Thomas <cyriellethomas@MacBook-Pro-de-Cyrielle-2.local>
2023-05-11 21:26:22 +0200

    modif avant push

--- frontend/src/pages/Musiques.jsx
+++ frontend/src/pages/Musiques.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React , { useState, useEffect } from 'react'
 
 const Musiques = () => {
+
+  useEffect(() => {
+    fetch("https://openwhyd.org/adrien/playlists?format=json")
+      .then((result) => result.json())
+      .then((musique) => {
+        console.log(musique);
+      });
+  }, []);
+  
   return (
     <div>Musiques</div>
   )
