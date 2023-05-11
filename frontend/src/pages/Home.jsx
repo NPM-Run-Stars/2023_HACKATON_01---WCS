27418b649387761b9d3f87303795bf2b46e161d6
Cyrielle Thomas <cyriellethomas@MacBook-Pro-de-Cyrielle-2.local>
2023-05-11 18:35:51 +0200

    Ajout icones Navbar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,56 +10,66 @@ import NavBar from "../components/Navbar";
 export default function Home() {
   return (
     <>
-    <header className="Header">
-      <div className="main-title">
-        <h1>Quel type de voyageur tu es?</h1>
-        <h3 className="second-title">Trouvez votre compagnon de voyage</h3>
-        <div className="logo">
-          <img className="logo" src={Logo} alt="city" width="200px" />
+      <header className="Header">
+        <div className="main-title">
+          <h1>Quel type de voyageur tu es?</h1>
+          <h3 className="second-title">Trouvez votre compagnon de voyage</h3>
+          <div className="logo">
+            <img className="logo" src={Logo} alt="city" width="200px" />
+          </div>
         </div>
-      </div>
-      <div className="pictures-container">
-        <div className="row-picture">
-          <div className="picture1">
-            <img
-              className="main-picture"
-              src={Montagne}
-              alt="city"
-              width="100%"
-            />
-            <div className="button-container">
-              <Link to="/" className="picture-button">
-                je suis actif
-              </Link>
+        <div className="pictures-container">
+          <div className="row-picture">
+            <div className="picture1">
+              <img
+                className="main-picture"
+                src={Montagne}
+                alt="city"
+                width="100%"
+              />
+              <div className="button-container">
+                <Link to="/" className="picture-button">
+                  je suis actif
+                </Link>
+              </div>
+            </div>
+            <div className="picture2">
+              <img
+                className="main-picture"
+                src={Plage}
+                alt="city"
+                width="100%"
+              />
+              <div className="button-container">
+                <Link to="/" className="picture-button">
+                  je suis actif
+                </Link>
+              </div>
             </div>
           </div>
-          <div className="picture2">
-            <img className="main-picture" src={Plage} alt="city" width="100%" />
-            <div className="button-container">
-              <Link to="/" className="picture-button">
-                je suis actif
-              </Link>
+          <div className="row-picture">
+            <div className="picture3">
+              <img
+                className="main-picture"
+                src={Concert}
+                alt="city"
+                width="100%"
+              />
+              <div className="button-container" />
+            </div>
+            <div className="picture4">
+              <img
+                className="main-picture"
+                src={City}
+                alt="city"
+                width="100%"
+              />
+              <div className="button-container" />
             </div>
           </div>
         </div>
-        <div className="row-picture">
-          <div className="picture3">
-            <img
-              className="main-picture"
-              src={Concert}
-              alt="city"
-              width="100%"
-            />
-            <div className="button-container" />
-          </div>
-          <div className="picture4">
-            <img className="main-picture" src={City} alt="city" width="100%" />
-            <div className="button-container" />
-          </div>
-        </div>
-      </div>
-    </header>
-    <NavBar />
+      </header>
+      <NavBar />
     </>
   );
 }
